646b97a78e3be502bd00b12562f69a3f064ecdd1
lee <ljyteach@163.com>
2017-03-21 15:56:34 +0800

    修改webpack配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+var path = require('path')
+var webpack = require('webpack')
+
 module.exports = {
     entry: './src/main.js',
     output: {
-        filename: './dist/bundle.js'
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'build.js'
     },
     module: {
         loaders: [{
@@ -9,22 +14,47 @@ module.exports = {
             loader: 'vue-loader'
         }, {
             test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
             loader: 'babel-loader',
+            exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
+        }, {
+            test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+            loader: 'file-loader'
+        }, {
+            test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+            loader: 'file-loader',
             query: {
-                presets: ['es2015']
+                name: '[name].[ext]?[hash]'
             }
         }]
-    },
-    vue: {
-        loaders: {
-            js: 'babel?{"presets":["es2015"]}',
-            css: 'vue-style!css'
-        }
     },
     resolve: {
         alias: {
             'vue$': 'vue/dist/vue.common.js'
         }
-    }
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true
+    },
+    devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+        // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ])
 }
