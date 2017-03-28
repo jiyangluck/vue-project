be14a8d8a41a4665a59114c9b0a188cc93f49ad6
lee <ljyteach@163.com>
2017-03-28 16:40:36 +0800

    x

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
+//自定义组件
 import App from './components/app.vue';
+import Home from './components/home.vue';
 import Movie from './components/movie.vue';
 import Videos from './components/videos.vue';
-import Home from './components/home.vue';
 import List from './components/list.vue';
 import Login from './components/login.vue';
-import VueResource from 'vue-resource';
+
 
 Vue.config.debug = true;
 Vue.use(VueRouter);
